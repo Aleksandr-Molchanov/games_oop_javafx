fa060cebbb8757eeba1dff70267ca523c9e4768e
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-22 09:25:49 +0300

    2. Сокобан. Тестовое задание. [#515890]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,9 +3,8 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean result = false;
-        int[] rsl = extractDiagonal(board);
-        for (int i = 0; i < rsl.length; i++) {
-            if (rsl[i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
                 result = true;
                 break;
             }
@@ -15,7 +14,7 @@ public class Win {
 
     public static boolean monoHorizontal(int[][] board, int row) {
         boolean rsl = true;
-        for (int i = 0; i < board[row].length; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[row][i] != 1) {
                 rsl = false;
                 break;
@@ -26,19 +25,11 @@ public class Win {
 
     public static boolean monoVertical(int[][] board, int column) {
         boolean rsl = true;
-        for (int i = 0; i < board[column].length; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][column] != 1) {
                 rsl = false;
                 break;
             }
-        }
-        return rsl;
-    }
-
-    public static int[] extractDiagonal(int[][] board) {
-        int[] rsl = new int[board.length];
-        for (int i = 0; i < rsl.length; i++) {
-            rsl[i] = board[i][i];
         }
         return rsl;
     }
