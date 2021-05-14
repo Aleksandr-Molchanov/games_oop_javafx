4282e0219d36f160346fbbc6783d3b63a4d5bd5f
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-14 15:07:06 +0300

    2. Каркас шахматной доски [#516047]

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.chess.firuges.black;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+
+
+public class BishopBlackTest {
+
+    @Test
+    public void positionTest() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.position(), is(Cell.C8));
+    }
+
+    @Test
+    public void copyTest() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.copy(Cell.A6).position(), is(Cell.A6));
+    }
+
+    @Test
+    public void wayTest() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] res = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(bishopBlack.way(Cell.G5), is(res));
+    }
+}
